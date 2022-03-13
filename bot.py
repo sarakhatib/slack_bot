79ee9ee3f7e3035787c2a8fd4c684a517ea02861
sarakhatib <khatibsara7@gmail.com>
2022-03-13 23:38:21 +0200

    first commit

---
+++ bot.py
@@ -0,0 +1,69 @@
+import os
+import time
+
+import github
+from slack_sdk.rtm_v2 import RTMClient
+from dotenv import load_dotenv
+from github import Github
+
+# from pprint import pprint
+
+load_dotenv()
+
+token = os.environ['GITHUB_TOKEN']
+g = Github(token)
+rtm = RTMClient(token=os.environ["SLACK_BOT_TOKEN"])
+github_user = g.get_user()
+# repos = user.get_repos()
+#
+# for repo in repos:
+#     print(repo.name)
+#     pull_requests = repo.get_pulls()
+#     for pull in pull_requests:
+#         print(pull.get_comments())
+
+
+@rtm.on("message")
+
+def handle(client: RTMClient, event: dict):
+    if 'Hello' in event['text']:
+        hello_handler(client, event)
+    elif 'Pull Request' in event['text'] or 'PR' in event['text']:
+        PR_handler(client,event)
+    else:
+        client.web_client.chat_postMessage(
+            channel=event['channel'],
+            text="nope"
+        )
+
+
+
+def hello_handler(client: RTMClient, event: dict):
+    channel_id = event['channel']
+    thread_ts = event['ts']
+    user = event['user']  # This is not username but user ID (the format is either U*** or W***)
+
+    client.web_client.chat_postMessage(
+        channel=channel_id,
+        text=f"Hi <@{user}>!"
+    )
+
+def PR_handler(client: RTMClient, event: dict):
+    channel_id = event['channel']
+    thread_ts = event['ts']
+    user = event['user']  # This is not username but user ID (the format is either U*** or W***)
+
+    client.web_client.chat_postMessage(
+        channel=channel_id,
+        text="send me a number"
+    )
+    time.sleep(15)
+    @rtm.on("message")
+    def response():
+        client.web_client.chat_postMessage(
+            channel=channel_id,
+            text="okay got it"
+        )
+
+
+rtm.start()
